9c656230a096af1aac917e4ff8b908a2fb28c295
goddenis <goddenis@yandex.ru>
2013-06-20 14:41:32 +0700

    ddd

--- grails-app/domain/test/Book.groovy
+++ grails-app/domain/test/Book.groovy
@@ -1,9 +1,11 @@
 package test
 
+import org.apache.commons.beanutils.converters.IntegerArrayConverter;
+
 class Book {
 
     String name
-    
+    Integer str
     static constraints = {
     }
 }
